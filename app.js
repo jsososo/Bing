299240c189c866517662618998f7c9191090cb79
jsososo <jsososo@outlook.com>
2020-10-28 19:02:39 +0800

    🐂 自适应图片尺寸

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.prepare().then(() => {
     } else {
       handle(req, res, parsedUrl)
     }
-  }).listen(3001, (err) => {
+  }).listen(3002, (err) => {
     if (err) throw err;
     updateData(true);
     console.log('> Ready on http://localhost:3001')
